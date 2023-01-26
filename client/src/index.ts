e895bed58de6824333c412c6069e3af5fccc0340
sleepyComrade <ivanovichivanivanov@protonmail.ch>
2023-01-27 05:06:22 +0900

    feat: install react

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello World!");
+import './style.css';
+import React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import {App} from './app';
+
+const root = ReactDOM.createRoot(document.body);
+root.render(React.createElement(App));
