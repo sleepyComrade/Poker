5dadc910f1e8f6856f751615cf2fb17def8986e2
max-romanov <max.romanov.90@mail.ru>
2023-02-03 19:26:23 +0200

    feat: add startGame method

--- server/src/room.ts
+++ server/src/room.ts
@@ -4,9 +4,21 @@ import { Player } from './player'
 export class Room implements IRoomServer {
   players: Record<string, Player>
   messages: string[]
+  isStarted: boolean
+  currentPlayerIndex: number
 
   constructor(public name: string) {
     this.players = {}
     this.messages = []
+  }
+
+  public startGame(): void {
+    if (this.isStarted) {
+      return
+    }
+
+    this.isStarted = true
+    const playersKeys = Object.keys(this.players)
+    console.log(playersKeys)
   }
 }
