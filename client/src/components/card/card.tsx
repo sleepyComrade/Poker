2df4d310be18aa7a4dc20674140fe0123444bb2d
Ramitsan <ramitsan@gmail.com>
2023-02-05 02:58:26 +0300

    fix: table cards styles

--- client/src/components/card/card.tsx
+++ client/src/components/card/card.tsx
@@ -12,7 +12,7 @@ export default function Card({ value, type }: CardProps) {
         <div className='card' style={{ width: "100%", height: "100%" }}>
             <div className='card_base card_a card_img' style={{ '--value': value, '--type': type }}>
             </div>
-            <div className='card_base card_b card_img' style={{ '--value': 2, '--type': 4 }}>
+            <div className='card_base card_b' style={{ '--value': 2, '--type': 4 }}>
             </div>
         </div>
     )
