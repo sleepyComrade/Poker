d924f1d20432df854f2b4d9e088e5e3b76f6a7f6
max-romanov <max.romanov.90@mail.ru>
2023-02-10 22:44:30 +0200

    fix: tabsize

--- server/src/room2.ts
+++ server/src/room2.ts
@@ -7,101 +7,101 @@ import { RoomLogic } from '../../client/src/game/room-logic';
 import { Player, BotPlayer } from '../../client/src/game/players';
 
 export class Room {
-    roomLogic: RoomLogic
-    messages: string[]
-    players: Map<connection, Player | BotPlayer> = new Map()
-    lastActions: IActions = null;
-    lastPlayer: Player = null;
+  roomLogic: RoomLogic
+  messages: string[]
+  players: Map<connection, Player | BotPlayer> = new Map()
+  lastActions: IActions = null;
+  lastPlayer: Player = null;
 
-    constructor(public name: string) {
-        this.roomLogic = new RoomLogic();
-        // setInterval(() => {
-        //     if(Math.random() < 0.02) {
-        //         const bot = new BotPlayer('bot' + Math.random() * 100000);
-        //         if (!this.roomLogic.checkTable()) {
-        //           this.roomLogic.join(bot);  
-        //         }
-        //     }
-        //     if(Math.random() < 0.02) {
-        //         const bot = [...this.players.values()].filter(it => it instanceof(BotPlayer))[0];
-        //         if(bot) {
-        //             this.roomLogic.leave(bot);
-        //         }
-        //     }
-        // }, 2000);    
-    }
+  constructor(public name: string) {
+    this.roomLogic = new RoomLogic();
+    // setInterval(() => {
+    //     if(Math.random() < 0.02) {
+    //         const bot = new BotPlayer('bot' + Math.random() * 100000);
+    //         if (!this.roomLogic.checkTable()) {
+    //           this.roomLogic.join(bot);  
+    //         }
+    //     }
+    //     if(Math.random() < 0.02) {
+    //         const bot = [...this.players.values()].filter(it => it instanceof(BotPlayer))[0];
+    //         if(bot) {
+    //             this.roomLogic.leave(bot);
+    //         }
+    //     }
+    // }, 2000);    
+  }
 
-    startGame(): void {
-        throw new Error('Method not implemented.')
-    }
-    handleMessage(connection: connection, msg: any, reqId?: string) {
-        switch(msg.type) {
-            case "join": {
-                const player = new Player(msg.data.name);
-                player.onMessage = (msg) => {
-                    if(msg.type == 'ask') {
-                        this.lastActions = msg.data.actions;
-                        this.lastPlayer = player;
-                        connection.sendUTF( JSON.stringify({
-                            type: 'pocker',
-                            data: {
-                              ...msg,
-                              data: {
-                                ...msg.data,
-                                actions: Object.keys(msg.data.actions)                
-                              }
-                            }
-                          }));
-                    } else {
-                        connection.sendUTF( JSON.stringify({
-                            type: 'pocker',
-                            data: {
-                              ...msg
-                            }
-                          }));
-                    }                    
+  startGame(): void {
+    throw new Error('Method not implemented.')
+  }
+  handleMessage(connection: connection, msg: any, reqId?: string) {
+    switch (msg.type) {
+      case "join": {
+        const player = new Player(msg.data.name);
+        player.onMessage = (msg) => {
+          if (msg.type == 'ask') {
+            this.lastActions = msg.data.actions;
+            this.lastPlayer = player;
+            connection.sendUTF(JSON.stringify({
+              type: 'pocker',
+              data: {
+                ...msg,
+                data: {
+                  ...msg.data,
+                  actions: Object.keys(msg.data.actions)
                 }
-                const playerIndex = this.roomLogic.join(player);
-                this.players.set(connection, player);
-                console.log("MSG JOIN",msg, "reqID", reqId)
-                connection.sendUTF(JSON.stringify({
-                    type: 'privateMessage',
-                    // requestId: "HelloWorld",
-                    data: {
-                        requestId: reqId,
-                        type: "join",
-                        playerIndex: playerIndex,
-                        roomName: this.name,
-                        succes: true
-                    }
-                }))
-                console.log("!!!!!!!!!!!!!!!!!!", this.name)
-                break;
-            }
-            case "leave": {
-                const currentPlayer = this.players.get(connection);
-                this.roomLogic.leave(currentPlayer);
-                this.players.delete(connection);
-                this.lastActions = null;
-                this.lastPlayer = null;
-                break;
-            }
-            case "move": {
-                const currentPlayer = this.players.get(connection);
-                if(currentPlayer == this.lastPlayer) {
-                    this.lastActions[msg.data.action as keyof IActions]();
-                    this.lastActions = null;
-                    this.lastPlayer = null;
-                }
-                break;
-            }
+              }
+            }));
+          } else {
+            connection.sendUTF(JSON.stringify({
+              type: 'pocker',
+              data: {
+                ...msg
+              }
+            }));
+          }
         }
-    }
-    handleDisconnect(connection: connection) {        
+        const playerIndex = this.roomLogic.join(player);
+        this.players.set(connection, player);
+        console.log("MSG JOIN", msg, "reqID", reqId)
+        connection.sendUTF(JSON.stringify({
+          type: 'privateMessage',
+          // requestId: "HelloWorld",
+          data: {
+            requestId: reqId,
+            type: "join",
+            playerIndex: playerIndex,
+            roomName: this.name,
+            succes: true
+          }
+        }))
+        console.log("!!!!!!!!!!!!!!!!!!", this.name)
+        break;
+      }
+      case "leave": {
         const currentPlayer = this.players.get(connection);
         this.roomLogic.leave(currentPlayer);
         this.players.delete(connection);
         this.lastActions = null;
         this.lastPlayer = null;
+        break;
+      }
+      case "move": {
+        const currentPlayer = this.players.get(connection);
+        if (currentPlayer == this.lastPlayer) {
+          this.lastActions[msg.data.action as keyof IActions]();
+          this.lastActions = null;
+          this.lastPlayer = null;
+        }
+        break;
+      }
     }
+  }
+  handleDisconnect(connection: connection) {
+    const currentPlayer = this.players.get(connection);
+    this.roomLogic.leave(currentPlayer);
+    this.players.delete(connection);
+    this.lastActions = null;
+    this.lastPlayer = null;
+  }
 }
